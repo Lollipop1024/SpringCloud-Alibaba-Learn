eef64ec4761ccdc82fba05f93607d592be7f8024
Lollipop <2607166703@qq.com>
2021-09-16 22:26:42 +0800

    支付框架搭建完成

---
+++ cloud-provider-payment/src/main/java/com/lollipop/springcloud/PaymentMain.java
@@ -0,0 +1,16 @@
+package com.lollipop.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Lollipop
+ * @Date: 2021-09-16 22:21
+ * @Description:
+ */
+@SpringBootApplication
+public class PaymentMain {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain.class);
+    }
+}
