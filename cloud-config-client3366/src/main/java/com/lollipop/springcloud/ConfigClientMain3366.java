baa7841ccca5628457801d1365e7f68dc4b6855d
jk <huanya.com123>
2021-09-27 16:03:15 +0800

    配置服务端实现手动刷新：curl -X POST "http://localhost:3344/actuator/bus-refresh"，同步所有客户端配置 配置服务端实现手动刷新：curl -X POST "http://localhost:3344/actuator/bus-refresh/服务名:端口"，同步指定客户端配置

---
+++ cloud-config-client3366/src/main/java/com/lollipop/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,19 @@
+package com.lollipop.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: J.K
+ * @Date: 2021-09-27 13:20
+ * @Description:
+ */
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
